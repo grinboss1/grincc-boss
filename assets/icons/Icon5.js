9bcb815c81e47967ccedd57a1cc7d7e028547c88
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 04:27:13 +1200

    Create Icon5.js

---
+++ assets/icons/Icon5.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Icon3 = () => {
+  return (
+    <img
+      src="https://i.ibb.co/mhcXpxR/mac.jpg"
+      alt="Icon 5"
+      style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+    />
+  );
+};
+
+export default Icon3;
