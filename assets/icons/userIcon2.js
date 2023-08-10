8dfbbf790f76794cdf051ab52e6a2d1f3d22b6b9
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-10 23:29:10 +1200

    Create userIcon2.js

---
+++ assets/icons/userIcon2.js
@@ -0,0 +1,9 @@
+// ../assets/icons/userIcon2.js
+
+import React from 'react';
+
+const UserIcon2 = () => {
+  return <img src="https://i.ibb.co/PhrSq2p/Capture.png" alt="User Icon" />;
+};
+
+export default UserIcon2;
