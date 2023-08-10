ae6acce6e93b85f8b29b63d60b6f56cc27125024
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-10 23:43:30 +1200

    Update userIcon2.js

--- assets/icons/userIcon2.js
+++ assets/icons/userIcon2.js
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const UserIcon2 = () => {
-  return <img src="https://i.ibb.co/PhrSq2p/Capture.png" alt="User Icon" style={{ width: '20px', height: '20px' }} />;
+  return <img src="https://i.ibb.co/PhrSq2p/Capture.png" alt="User Icon" style={{ width: '20px', height: '20px', marginLeft: '2px' }} />;
 };
 
 export default UserIcon2;
