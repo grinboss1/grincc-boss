bb07248681eeb171c851d2125c126a6d20f4f111
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 04:29:29 +1200

    Create Icon6.js

---
+++ assets/icons/Icon6.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Icon3 = () => {
+  return (
+    <img
+      src="https://i.ibb.co/VMsjm9f/mw-grin.png"
+      alt="Icon 6"
+      style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+    />
+  );
+};
+
+export default Icon3;
