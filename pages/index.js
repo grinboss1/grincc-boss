e6aa82052d088c93e4039c5518d0f023e75ac98f
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 03:11:09 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,9 @@ function Index(props) {
 
    <ul className="pl-4 pb-4">
   <li style={{ display: 'flex', alignItems: 'center' }}>
-    <div style={{ display: 'flex', alignItems: 'center', border: '1.5px solid #000', padding: '2px', borderRadius: '5px', marginTop: '1px', marginBottom: '0px', marginLeft: '-18px' }}>
+    <div style={{ display: 'flex', alignItems: 'center', border: '1.5px solid #000', padding: '2px', borderRadius: '5px', marginTop: '1px', marginBottom: '0px', marginLeft: '-3.5px' }}>
       <UserIcon2 />
-      <span style={{ paddingLeft: '1.6px', marginBottom: '1px', marginRight: '2px' }}>grin boss <span style={{ fontSize: '0.7em', color: '#888' }}>*CC + OC lead*</span></span>
+      <span style={{ paddingLeft: '3.6px', marginBottom: '4.6px', marginRight: '2px' }}>grin boss <span style={{ fontSize: '0.7em', color: '#888' }}>*CC + OC lead*</span></span>
     </div>
   </li>
   <li style={{ display: 'flex', alignItems: 'center' }}>
