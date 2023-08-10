51ec56834b94b86bbb86d1c29788c2a9388ecc45
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 04:21:23 +1200

    Create Icon4.js

---
+++ assets/icons/Icon4.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Icon3 = () => {
+  return (
+    <img
+      src="https://i.ibb.co/SX5bMQw/mike.jpg"
+      alt="Icon 4"
+      style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+    />
+  );
+};
+
+export default Icon3;
