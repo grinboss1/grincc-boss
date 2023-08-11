4bb68d42275dbeda220cba091ef33a99b8ebbf5f
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 13:31:37 +1200

    Update Icon4.js

--- assets/icons/Icon4.js
+++ assets/icons/Icon4.js
@@ -4,7 +4,7 @@ const Icon4 = ({ className }) => {
   return (
     <img
       className={className} // Added className prop
-      src="https://i.ibb.co/SX5bMQw/mike.jpgg"
+      src="https://i.ibb.co/SX5bMQw/mike.jpg"
       alt="Icon 4"
       style={{ width: '24px', height: '24px', borderRadius: '50%' }}
     />
