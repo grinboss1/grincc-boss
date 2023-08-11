e0244aa43aeb198cabb5a73fd581f052ae3eabe0
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 14:27:39 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -22,3 +22,24 @@ export const Icon4 = ({ className }) => {
   );
 };
 
+export const Icon5 = ({ className }) => {
+  return (
+    <img
+      className={className}
+      src="https://i.ibb.co/mhcXpxR/mac.jpg"
+      alt="Icon 5"
+      style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+    />
+  );
+};
+
+export const Icon6 = ({ className }) => {
+  return (
+    <img
+      className={className}
+      src="https://i.ibb.co/VMsjm9f/mw-grin.png"
+      alt="Icon 6"
+      style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+    />
+  );
+};
