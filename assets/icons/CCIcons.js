fce304059fb9b9a37fc1fbc11dc08bf2f437b4a8
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 14:58:33 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-export const Icon3 = ({ className = "pl-4 py-2" }) => {
+export const Icon3 = () => {
   return (
     <img
-      className={className}
       src="https://i.ibb.co/Bj70xwG/anon.jpg"
       alt="Icon 3"
       style={{ width: '24px', height: '24px', borderRadius: '50%' }}
