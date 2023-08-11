241389d0e63a24c83acc8304de48837e06a0aab5
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 13:55:16 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,26 +51,7 @@ function Index(props) {
 </li>
 
   
-      <li style={{ display: 'flex', alignItems: 'center', marginTop: '0px' }}>
-        <div style={{ display: 'flex', alignItems: 'center', padding: '2px', marginTop: '1px', marginBottom: '-2px', marginLeft: '-2px' }}>
-          <Icon4 />
-          <span style={{ paddingLeft: '3.4px', marginBottom: '1px', marginRight: '2px' }}>mcmmike <span style={{ fontSize: '0.7em', color: '#888' }}></span></span>
-        </div>
-      </li>
-      <li style={{ display: 'flex', alignItems: 'center', marginTop: '0px' }}>
-        <div style={{ display: 'flex', alignItems: 'center', padding: '2px', marginTop: '1px', marginBottom: '-2px', marginLeft: '-2px' }}>
-          <Icon5 />
-          <span style={{ paddingLeft: '3.4px', marginBottom: '1px', marginRight: '2px' }}>future3000 <span style={{ fontSize: '0.7em', color: '#888' }}></span></span>
-        </div>
-      </li>
-      <li style={{ display: 'flex', alignItems: 'center', marginTop: '0px' }}>
-        <div style={{ display: 'flex', alignItems: 'center', padding: '2px', marginTop: '1px', marginBottom: '-2px', marginLeft: '-2px' }}>
-          <Icon6 />
-          <span style={{ paddingLeft: '3.4px', marginBottom: '1px', marginRight: '2px' }}>mwgrin_fr <span style={{ fontSize: '0.7em', color: '#888' }}></span></span>
-        </div>
-      </li>
-    </ul>
-  </div>
+    
 
 
      
