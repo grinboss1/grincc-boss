60e473f4f80d327b84d16a7d1753e9d6f4109f55
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 13:12:22 +1200

    Update Icon3.js

--- assets/icons/Icon3.js
+++ assets/icons/Icon3.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Icon3 = () => {
+const Icon3 = ({ className }) => {
   return (
     <img
+      className={className} // Added className prop
       src="https://i.ibb.co/Bj70xwG/anon.jpg"
       alt="Icon 3"
       style={{ width: '24px', height: '24px', borderRadius: '50%' }}
