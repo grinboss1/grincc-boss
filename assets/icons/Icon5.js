4e13e3d7e8c8d451a3b2e66c2150539c9bd0cbfe
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 13:35:34 +1200

    Update Icon5.js

--- assets/icons/Icon5.js
+++ assets/icons/Icon5.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Icon3 = () => {
+const Icon3 = ({ className }) => {
   return (
     <img
+      className={className} // Added className prop
       src="https://i.ibb.co/mhcXpxR/mac.jpg"
-      alt="Icon 5"
+      alt="Icon 3"
       style={{ width: '24px', height: '24px', borderRadius: '50%' }}
     />
   );
 };
 
 export default Icon3;
+
+
+
+
