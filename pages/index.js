2bc72f1a16ee4edbe43c8de0af08825e17a6a9e9
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-11 14:56:30 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ function Index(props) {
         <p>Grin Community Council is <a className="text-primary" href="https://forum.grin.mw/t/call-for-grin-community-candidates-for-additional-fund-granting-control/8521/13">formed</a> from valued community members with remarkable contributions to Grin in 2021 May.
           Council's main task is to control and direct <i>Grin Community Fund </i> to support grin projects, bounties and community activities. Current council members are:</p>
 
- <div className="py-4">
+<div className="py-4">
   <div className="icons-container">
     <ul className="pl-4 pb-4" style={{ margin: '0px', padding: '0px' }}>
       <li className="icon-container">
-        <div className="icon-wrapper px-2 py-2"> {/* Apply padding here */}
+        <div className="icon-wrapper">
           <Icon3 />
           <span className="icon-label">anynomous <span></span></span>
         </div>
