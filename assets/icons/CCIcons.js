45a29946bde7511a00650595c4039650114c810c
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-13 18:57:37 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -29,26 +29,38 @@ export const IconWrapper = ({ IconComponent, label }) => {
   const iconRef = useRef(null);
 
  const openModal = () => {
-    const rect = iconRef.current.getBoundingClientRect();
-setModalPosition({ top: rect.top + window.scrollY, left: rect.right });
-    setModalIsOpen(true);
-  };
+  const rect = iconRef.current.getBoundingClientRect();
+  console.log("Icon position:", rect);
+  const modalWidth = 200; // The width of your modal
+  let leftPosition = rect.right;
+
+  // Check if the modal would go off the right side of the viewport
+  if (leftPosition + modalWidth > window.innerWidth) {
+    leftPosition = rect.left - modalWidth; // Position the modal to the left of the icon
+  }
+
+  console.log("Modal left position:", leftPosition);
+
+  setModalPosition({ top: rect.top + window.scrollY, left: leftPosition });
+  setModalIsOpen(true);
+};
+
+
+
 
   const closeModal = () => setModalIsOpen(false);
 
-return (
-  <ul className="icon-list">
-    <div className="icon-parent-container" style={{ position: 'relative' }}>
-      <li ref={iconRef} className="icon-container px-2 py-2 relative" onClick={openModal} style={{ cursor: 'pointer' }}>
-        <div className="icon-wrapper">
+  return (
+    <li className="icon-container px-2 py-2 relative" onClick={openModal} style={{ cursor: 'pointer' }}>
+      <div className="icon-wrapper">
+        <div ref={iconRef}> {/* Set the ref here */}
           <IconComponent />
-          <span className={`icon-label ${modalIsOpen ? 'highlighted' : ''}`}>{label}</span>
         </div>
-      </li>
+        <span className={`icon-label ${modalIsOpen ? 'highlighted' : ''}`}>{label}</span>
+      </div>
       {modalIsOpen && <CustomModal position={modalPosition} onClose={closeModal} label={label} />}
-    </div>
-  </ul>
-);
+    </li>
+  );
 };
 
 const CustomModal = ({ onClose, label, position }) => {
@@ -68,7 +80,7 @@ const CustomModal = ({ onClose, label, position }) => {
   }, [onClose]);
 
   return (
-    <div className="modal-content-container" style={{ top: position.top, left: position.left }}>
+    <div className="modal-content-container" style={{ position: 'absolute', top: position.top, left: position.left }}>
       <div className="modal-content" ref={contentRef}>
         <p>Visit the <a href="https://www.grin.mw" target="_blank" rel="noopener noreferrer">forum</a></p>
       </div>
