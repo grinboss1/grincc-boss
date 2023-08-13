b8839c371f37addca240274e256620dbb0877fdc
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-13 19:46:16 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -34,7 +34,8 @@ export const IconWrapper = ({ icon, label }) => {
     const rect = iconRef.current.getBoundingClientRect();
     console.log('Rect values:', rect); // Log the entire rect object
     const topPosition = rect.top + window.scrollY;
-    const leftPosition = rect.right;
+    const leftPosition = rect.right + 10; // Add 10 pixels to the right
+
     console.log('Calculated top:', topPosition, 'Calculated left:', leftPosition); // Log the calculated positions
     setModalPosition({ top: topPosition, left: leftPosition });
     setModalIsOpen(true);
