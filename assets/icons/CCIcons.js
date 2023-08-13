665cf0c8b54f3f53365540d27d006472d4e53389
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-13 16:48:09 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -7,15 +7,18 @@ const icons = [
   { src: "https://i.ibb.co/VMsjm9f/mw-grin.png", alt: "Icon 6", label: "mwgrin_fr" }
 ];
 
+export const Icon = ({ src, alt }) => (
+  <img
+    src={src}
+    alt={alt}
+    style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+  />
+);
+
 export const IconList = () => (
   <ul className="icon-list">
     {icons.map((icon, index) => (
-      <li className="icon-container" key={index}>
-        <div className="icon-wrapper">
-          <img src={icon.src} alt={icon.alt} style={{ width: '24px', height: '24px', borderRadius: '50%' }} />
-          <span className="icon-label">{icon.label}</span>
-        </div>
-      </li>
+      <IconWrapper IconComponent={() => <Icon src={icon.src} alt={icon.alt} />} label={icon.label} key={index} />
     ))}
   </ul>
 );
