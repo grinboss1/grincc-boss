b8dabe7a5cfb8326ff6a920a67bebdf1a073ec74
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-14 10:52:38 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -29,63 +29,56 @@ export const IconWrapper = ({ icon, label }) => {
   const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
   const iconAndLabelRef = useRef(null);
 
- const openModal = () => {
-  if (iconAndLabelRef.current) {
+  const openModal = () => {
     const rect = iconAndLabelRef.current.getBoundingClientRect();
     const topPosition = rect.top + window.scrollY;
-    const leftPosition = window.innerWidth <= 768 ? rect.left : rect.right; // Example: Adjust based on screen size
+    const leftPosition = rect.right + 20;
     setModalPosition({ top: topPosition, left: leftPosition });
     setModalIsOpen(true);
     document.body.style.overflow = 'hidden'; // Prevent scrolling
-  }
-};
-
-
-const closeModal = () => {
-  setModalIsOpen(false);
-  document.body.style.overflow = 'auto'; // Allow scrolling
-};
-
-useEffect(() => {
-  const handleClickOutside = (event) => {
-    if (
-      modalIsOpen &&
-      iconAndLabelRef.current &&
-      !iconAndLabelRef.current.contains(event.target)
-    ) {
-      closeModal();
-    }
   };
-  document.addEventListener('mousedown', handleClickOutside);
-  return () => {
-    document.removeEventListener('mousedown', handleClickOutside);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    document.body.style.overflow = 'auto'; // Allow scrolling
   };
-}, [modalIsOpen]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalIsOpen && iconAndLabelRef.current && !iconAndLabelRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modalIsOpen]);
 
   return (
-  <li className="icon-container px-1 py-1 relative" style={{ marginLeft: '0.5rem' }}>
-    <div className="icon-parent-container" style={{ position: 'relative', padding: '0px' }}>
-      <div ref={iconAndLabelRef} onClick={openModal} className="icon-wrapper">
-        <Icon src={icon.src} alt={icon.alt} />
-        <span className={`icon-label ${modalIsOpen ? 'icon-label-bold' : ''}`}>{label}</span>
+    <li className="icon-container px-1 py-1 relative" style={{ marginLeft: '0.5rem' }}>
+      <div className="icon-parent-container" style={{ position: 'relative', padding: '0px' }}>
+        <div ref={iconAndLabelRef} onClick={openModal} style={{ cursor: 'pointer', display: 'inline-flex', alignItems: 'center' }}>
+          <Icon src={icon.src} alt={icon.alt} />
+          <span className={`icon-label ${modalIsOpen ? 'icon-label-bold' : ''}`} style={{ minWidth: '100px' }}>{label}</span> {/* Set min-width as needed */}
+        </div>
+        {modalIsOpen && <CustomModal position={modalPosition} onClose={closeModal} label={label} />}
       </div>
-      {modalIsOpen && <CustomModal position={modalPosition} onClose={closeModal} label={label} />}
-
-    </div>
-  </li>
-);
-
+    </li>
+  );
 };
 
-const CustomModal = ({ position, onClose, label }) => {
+
+
+const CustomModal = ({ onClose, label, position }) => {
   return (
     <div
       className="modal-content-container"
       style={{
         position: 'fixed',
         top: position.top,
-        left: position.left,
-        zIndex: 10000,
+        left: position.left, // Using left position instead of right
+        zIndex: 10000
       }}
       onClick={(e) => e.stopPropagation()}
     >
