b211c08311ab82f94deae21c4e0f3f7d2dc460e8
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-13 23:08:40 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -61,7 +61,8 @@ export const IconWrapper = ({ icon, label }) => {
  return (
     <ul className="icon-list" style={{ padding: 0, margin: 0 }}>
       <div className="icon-parent-container" style={{ position: 'relative', padding: '0px' }}>
-        <li className="icon-container px-2 py-2 relative" style={{ margin: '0px' }}>
+        <li className="icon-container px-1 py-1 relative" style={{ margin: '0px' }}>
+
           {/* Wrap the icon and label inside a div and attach the click event to this div */}
           <div ref={iconAndLabelRef} onClick={openModal} style={{ cursor: 'pointer', display: 'inline-flex', alignItems: 'center' }}>
             <Icon src={icon.src} alt={icon.alt} />
