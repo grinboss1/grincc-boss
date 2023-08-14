805387a5acff63bf0d835a104ba6c754f8424da4
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-15 03:37:34 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -69,14 +69,14 @@ export const IconWrapper = ({ icon, label }) => {
         </div>
         {modalIsOpen && (
           <>
-            <div className="background-popup"></div> {/* Use the new class */}
-            <div className="modal-content-container" style={{ position: 'absolute', top: '50%', left: '100%', zIndex: 10000, transform: 'translateY(-50%)' }}>
-              <div className="modal-content">
-                <p>{details.text} <a href={details.url} target="_blank" rel="noopener noreferrer">{details.url}</a></p>
-              </div>
-            </div>
-          </>
-        )}
+            <div className="background-popup popup-common-style"></div> {/* Background Popup */}
+    <div className="modal-content-container popup-common-style" style={{ position: 'absolute', top: '50%', left: '100%', zIndex: 10000, transform: 'translateY(-50%)' }}>
+      <div className="modal-content">
+        <p>{details.text} <a href={details.url} target="_blank" rel="noopener noreferrer">{details.url}</a></p>
+      </div>
+    </div>
+  </>
+)}
       </div>
     </li>
   );
