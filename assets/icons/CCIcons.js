98db2f7e45b3d6297902a7a68c1d729ab0db2bd9
grinboss1 <141922285+grinboss1@users.noreply.github.com>
2023-08-15 14:59:52 +1200

    Update CCIcons.js

--- assets/icons/CCIcons.js
+++ assets/icons/CCIcons.js
@@ -1,5 +1,6 @@
 
 
+
 import React, { useState, useRef, useEffect } from 'react';
 
 const icons = [
@@ -70,12 +71,12 @@ export const IconWrapper = ({ icon, label }) => {
         </div>
         {modalIsOpen && (
           <>
-            <div className="background-popup" style={{ left: '0', top: '0' }}></div> {/* Background Popup */}
-            <div className="modal-content-container"> {/* Content Popup */}
-              <div className="modal-content">
-                <p>{details.text} <a href={details.url} target="_blank" rel="noopener noreferrer">{details.url}</a></p>
-              </div>
-            </div>
+            <div className="background-popup popup-common-style"></div>
+<div className="modal-content-container popup-common-style">
+  <div className="modal-content">
+    <p>{details.text} <a href={details.url} target="_blank" rel="noopener noreferrer">{details.url}</a></p>
+  </div>
+</div>
           </>
         )}
       </div>
